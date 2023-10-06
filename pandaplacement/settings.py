9f7b74c7e8d0844c1787aa592a04768c31c9cc1e
HakimAhmadi1 <133155927+HakimAhmadi1@users.noreply.github.com>
2023-10-06 20:03:55 +0500

    changed drf sessions

--- pandaplacement/settings.py
+++ pandaplacement/settings.py
@@ -110,18 +110,18 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
 ]
 
-# REST_FRAMEWORK = {
-#     'DEFAULT_AUTHENTICATION_CLASSES': (
-#         'rest_framework.authentication.SessionAuthentication',  
-#         # 'rest_framework.authentication.TokenAuthentication',  
-#     ),
-#     'DEFAULT_PERMISSION_CLASSES': (
-#         'rest_framework.permissions.IsAuthenticated',
-#     ),
-#     'DEFAULT_RENDERER_CLASSES': [
-#         'rest_framework.renderers.JSONRenderer',
-#     ],
-# }
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',  
+        # 'rest_framework.authentication.TokenAuthentication',  
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
+}
 
 
 PASSWORD_RESET_TIMEOUT = 1800
